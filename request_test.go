b5e406af74fbd6fb566da442bb9ec7c97fd0a979
Victor Popkov <victor@popkov.me>
2017-05-12 18:06:22 +0300

    Add Request tests

---
+++ request_test.go
@@ -0,0 +1,35 @@
+package appcast
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewRequest(t *testing.T) {
+	r, err := NewRequest("http://example.com/")
+	assert.Nil(t, err)
+	assert.IsType(t, Request{}, *r)
+	assert.Equal(t, "GET", r.HTTPRequest.Method)
+	assert.Equal(t, "http://example.com/", r.HTTPRequest.URL.String())
+
+	// invalid URL should return error
+	r, err = NewRequest("http://192.168.0.%31/")
+	assert.Nil(t, r)
+	assert.Error(t, err)
+}
+
+func TestAddHeader(t *testing.T) {
+	r, _ := NewRequest("http://example.com/")
+
+	// before
+	assert.Len(t, r.HTTPRequest.Header, 0)
+
+	// add header
+	r.AddHeader("User-Agent", "Example")
+
+	// after
+	headers := r.HTTPRequest.Header
+	assert.Len(t, headers, 1)
+	assert.Equal(t, "Example", headers.Get("User-Agent"))
+}
