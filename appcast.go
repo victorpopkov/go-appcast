7bea087a697697fcb28451cab7e7a0548a7c1ea2
Victor Popkov <victor@popkov.me>
2017-05-12 06:05:09 +0300

    Update appcast package description

--- appcast.go
+++ appcast.go
@@ -1,2 +1,11 @@
-// Package appcast provides functionality for working with appcasts.
+// Package appcast provides functionality for working with appcasts to retrieve
+// valuable information about software releases.
+//
+// Currently supports 3 providers:
+//
+// – Sparkle RSS Feed
+// – SourceForge RSS Feed
+// – GitHub Atom Feed
+//
+// See README.md for more info.
 package appcast
