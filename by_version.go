da649b08f96bbb00f8f4a4cddcacb23832402f20
Victor Popkov <victor@popkov.me>
2017-05-25 02:05:04 +0300

    Add ByVersion with tests

---
+++ by_version.go
@@ -0,0 +1,16 @@
+package appcast
+
+// ByVersion implements sort.Interface for []Release based on the Version field.
+type ByVersion []Release
+
+func (a ByVersion) Len() int {
+	return len(a)
+}
+
+func (a ByVersion) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func (a ByVersion) Less(i, j int) bool {
+	return a[i].Version.LessThan(a[j].Version)
+}
---
+++ by_version_test.go
@@ -0,0 +1,51 @@
+package appcast
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func createTestByVersionReleases() (result []Release) {
+	testReleases := [][]string{
+		{"1.0.0", "100"},
+		{"1.1.0", "110"},
+		{"2.0.0", "200"},
+	}
+
+	for _, release := range testReleases {
+		r, _ := NewRelease(release[0], release[1])
+		result = append(result, *r)
+	}
+
+	return result
+}
+
+func TestByVersionLen(t *testing.T) {
+	// preparations
+	testReleases := createTestByVersionReleases()
+
+	// test
+	assert.Equal(t, 3, ByVersion(testReleases).Len())
+}
+
+func TestByVersionSwap(t *testing.T) {
+	// preparations
+	testReleases := createTestByVersionReleases()
+
+	// test
+	assert.Equal(t, "1.1.0", testReleases[1].Version.String())
+	assert.Equal(t, "2.0.0", testReleases[2].Version.String())
+	ByVersion(testReleases).Swap(1, 2)
+	assert.Equal(t, "2.0.0", testReleases[1].Version.String())
+	assert.Equal(t, "1.1.0", testReleases[2].Version.String())
+}
+
+func TestByVersionLess(t *testing.T) {
+	// preparations
+	testReleases := createTestByVersionReleases()
+
+	// test
+	assert.True(t, ByVersion(testReleases).Less(0, 1))
+	assert.True(t, ByVersion(testReleases).Less(1, 2))
+}
