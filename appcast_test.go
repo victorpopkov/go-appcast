a4b5f02ff801891ce76de3cff393f4876b4b6a00
Victor Popkov <victor@popkov.me>
2017-05-25 06:44:44 +0300

    Move ExampleSparkleRSSFeed into separate example file

--- appcast_test.go
+++ appcast_test.go
@@ -68,37 +68,6 @@ func getLineFromString(lineNum int, content string) (line string, err error) {
 	return readLine(r, lineNum)
 }
 
-func ExampleSparkleRSSFeed() {
-	// mock the requests
-	content := string(getTestdata("example_sparkle.xml"))
-	httpmock.ActivateNonDefault(DefaultClient.HTTPClient)
-	httpmock.RegisterResponder("GET", "https://www.adium.im/sparkle/appcast-release.xml", httpmock.NewStringResponder(200, content))
-	defer httpmock.DeactivateAndReset()
-
-	// example
-	a := New()
-	a.LoadFromURL("https://www.adium.im/sparkle/appcast-release.xml")
-	a.GenerateChecksum(Sha256)
-	a.ExtractReleases()
-	a.SortReleasesByVersions(DESC)
-
-	fmt.Println("Checksum:", a.GetChecksum())
-	fmt.Println("Provider:", a.Provider)
-
-	for i, release := range a.Releases {
-		fmt.Println(fmt.Sprintf("Release #%d:", i+1), release)
-	}
-
-	// Output:
-	// Checksum: bfee8d59301ff64b44d72572e973a8354d0397657552c91db61bedac353b04b8
-	// Provider: Sparkle RSS Feed
-	// Release #1: {1.5.10.4 1.5.10.4 Adium 1.5.10.4  [{https://adiumx.cachefly.net/Adium_1.5.10.4.dmg application/octet-stream 21140435}] 2017-05-14 05:04:01 -0700 -0700 false}
-	// Release #2: {1.5.10 1.5.10 Adium 1.5.10  [{https://adiumx.cachefly.net/Adium_1.5.10.dmg application/octet-stream 24595712}] 0001-01-01 00:00:00 +0000 UTC false}
-	// Release #3: {1.4.5 1.4.5 Adium 1.4.5  [{https://adiumx.cachefly.net/Adium_1.4.5.dmg application/octet-stream 23065688}] 0001-01-01 00:00:00 +0000 UTC false}
-	// Release #4: {1.3.10 1.3.10 Adium 1.3.10  [{https://adiumx.cachefly.net/Adium_1.3.10.dmg application/octet-stream 22369877}] 0001-01-01 00:00:00 +0000 UTC false}
-	// Release #5: {1.0.6 1.0.6 Adium 1.0.6  [{https://adiumx.cachefly.net/Adium_1.0.6.dmg application/octet-stream 13795246}] 0001-01-01 00:00:00 +0000 UTC false}
-}
-
 func TestNew(t *testing.T) {
 	a := New()
 	assert.IsType(t, BaseAppcast{}, *a)
