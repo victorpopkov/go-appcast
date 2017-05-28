87db5a3e8a66657b49672f9e585cb0ad3c453ba0
Victor Popkov <victor@popkov.me>
2017-05-28 09:21:19 +0300

    Add GitHubAtomFeedAppcast tests

---
+++ github_atom_feed_test.go
@@ -0,0 +1,51 @@
+package appcast
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGitHubAtomFeedAppcastExtractReleases(t *testing.T) {
+	testCases := map[string]map[string][]string{
+		"github_default.xml": {
+			"2.0.0": {"2016-05-13 12:00:00 +0200 +0200", "https://sourceforge.net/projects/example/files/app/2.0.0/app_2.0.0.dmg/download"},
+			"1.1.0": {"2016-05-12 12:00:00 +0200 +0200", "https://sourceforge.net/projects/example/files/app/1.1.0/app_1.1.0.dmg/download"},
+			"1.0.1": {"2016-05-11 12:00:00 +0200 +0200", "https://sourceforge.net/projects/example/files/app/1.0.1/app_1.0.1.dmg/download"},
+			"1.0.0": {"2016-05-10 12:00:00 +0200 +0200", "https://sourceforge.net/projects/example/files/app/1.0.0/app_1.0.0.dmg/download"},
+		},
+	}
+
+	errorTestCases := map[string]string{}
+
+	// test (successful)
+	for filename, releases := range testCases {
+		// preparations
+		a := new(GitHubAtomFeedAppcast)
+		a.Content = string(getTestdata(filename))
+
+		// test
+		assert.Empty(t, a.Releases)
+		err := a.ExtractReleases()
+		assert.Nil(t, err)
+		assert.Len(t, a.Releases, len(releases))
+		for _, release := range a.Releases {
+			v := release.Version.String()
+			assert.Equal(t, fmt.Sprintf("%s", v), release.Title)
+			assert.Equal(t, releases[v][0], release.PublishedDateTime.String())
+		}
+	}
+
+	// test (error)
+	for filename, errorMsg := range errorTestCases {
+		// preparations
+		a := new(GitHubAtomFeedAppcast)
+		a.Content = string(getTestdata(filename))
+
+		// test
+		err := a.ExtractReleases()
+		assert.Error(t, err)
+		assert.Equal(t, errorMsg, err.Error())
+	}
+}
