3867d87a716cea2e4ba05269405de88f7c40cd62
Victor Popkov <victor@popkov.me>
2018-09-09 21:41:04 +0300

    Add Output tests

---
+++ output_test.go
@@ -0,0 +1,63 @@
+package appcast
+
+import (
+	"encoding/hex"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+// newTestOutput creates a new Output instance for testing purposes and returns
+// its pointer.
+func newTestOutput() *Output {
+	content := []byte("test")
+	o := &Output{
+		content: content,
+		checksum: &Checksum{
+			algorithm: SHA256,
+			source:    content,
+			result:    []byte("test"),
+		},
+		provider: Unknown,
+	}
+
+	return o
+}
+
+func TestOutput_Save(t *testing.T) {
+	o := newTestOutput()
+	assert.Panics(t, func() {
+		o.Save()
+	})
+}
+
+func TestOutput_GenerateChecksum(t *testing.T) {
+	// preparations
+	o := newTestOutput()
+	assert.Equal(t, hex.EncodeToString([]byte("test")), o.Checksum().String())
+
+	// test
+	o.GenerateChecksum(SHA256)
+	assert.Equal(t, "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08", o.Checksum().String())
+}
+
+func TestOutput_Content(t *testing.T) {
+	o := newTestOutput()
+	assert.Equal(t, []byte("test"), o.Content())
+}
+
+func TestOutput_SetContent(t *testing.T) {
+	o := newTestOutput()
+	o.SetContent([]byte("new test"))
+	assert.Equal(t, []byte("new test"), o.content)
+}
+
+func TestOutput_Provider(t *testing.T) {
+	o := newTestOutput()
+	assert.Equal(t, Unknown, o.Provider())
+}
+
+func TestOutput_SetProvider(t *testing.T) {
+	o := newTestOutput()
+	o.SetProvider(SparkleRSSFeed)
+	assert.Equal(t, SparkleRSSFeed, o.provider)
+}
